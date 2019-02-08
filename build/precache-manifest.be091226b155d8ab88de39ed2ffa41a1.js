feeb5ed3513fd9ae5d472b1b75e8417be2c80d21
Falendra Bandhe <sahilbandhe@gmail.com>
2019-02-08 17:28:57 +0530

    build was missing

---
+++ build/precache-manifest.be091226b155d8ab88de39ed2ffa41a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed1d2a557dbb038da12a",
+    "url": "/static/css/main.e271f51f.chunk.css"
+  },
+  {
+    "revision": "ed1d2a557dbb038da12a",
+    "url": "/static/js/main.ed1d2a55.chunk.js"
+  },
+  {
+    "revision": "a6ca65584a1a91f1d3ba",
+    "url": "/static/js/1.a6ca6558.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f77ce998df80e4703f13fc0cf2907046",
+    "url": "/index.html"
+  }
+];
